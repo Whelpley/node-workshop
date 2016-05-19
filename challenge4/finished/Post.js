de04977ece358f2463cbf8f5e8af0a81361c5fd6
Whelpley <whelpley@gmail.com>
2016-05-19 10:47:02 -0700

    Challenge 4 in progress

--- challenge4/finished/Post.js
+++ challenge4/finished/Post.js
@@ -2,7 +2,7 @@
 var mongoose = require('mongoose');
 
 // Configure conenction URL (only needs to happen once per app)
-mongoose.connect('CHANGEME');
+mongoose.connect('mongodb://whelpley:doggod@ds025742.mlab.com:25742/node_intro_sandbox');
 
 // Create a database schema for our Post object
 var postSchema = mongoose.Schema({
